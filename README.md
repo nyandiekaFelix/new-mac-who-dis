55e35a00254cfbe2a2d695242abab364308f7cf4
nyandiekaFelix <nyandiekafelix@gmail.com>
2019-02-11 19:37:39 +0300

    add dev environment configs

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## My Dotfiles
+## My MacOS Dotfiles
 
 
--- setup.sh
+++ setup.sh
@@ -1,43 +1,32 @@
 #! /bin/bash
 
+COLOR="\033[1;32m"
+NO_COLOR="\033[0m"
 
-# Determine which OS the configurations are going to run in
-function get_package_manager() {
-  # MAC
-  which brew > /dev/null 2>&1 && {
-    export PACKAGE_MANAGER="homebrew"
-    return;
-  }
+function config_brew() {
+  echo -e "\n${COLOR}_____ Setting up Homebrew _____${NO_COLOR}\n"
 
-  # UBUNTU
-  which apt > /dev/null 2>&1 && {
-    export PACKAGE_MANAGER="apt"
-    return;
-  }
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
 
 function config_zsh() {
-  ZSH_DIR=~/.oh-my-zsh
+  echo -e "\n${COLOR}_____ Setting up ZSH _____${NO_COLOR}\n"
 
-  if [ -w "$ZSH_DIR" ] ; then
-    echo "_____ Updating ZSH _____\n"
-    cd "$ZSH_DIR"
-    git pull origin master
-    cd ~/
-  else
-    echo "_____ Installing ZSH _____\n"
-    brew install zsh
-    echo "__ Setting ZSH as default shell __\n"
-    chsh -s /usr/local/bin/zsh
-    # To do - Ubuntu configs
-  fi
+  ZSH_DIR="~/.oh-my-zsh"
+
+  brew install zsh
+  chsh -s /usr/local/bin/zsh
+
+  # set zsh theme
+  cp ./zsh-themes/* "${ZSH_DIR}/custom/themes"
+  sed -i '' -e 's/ZSH_THEME=.*/ZSH_THEME="nt9"/' ~/.zshrc
 }
 
 function config_git() {
-  echo "_____ Setting up Git _____"
+  echo -e "\n${COLOR}_____ Setting up Git _____${NO_COLOR}\n"
 
-  git config --global user.email $GIT_EMAIL
-  git config --global user.name $GIT_USERNAME
+  git config --global user.email ${GIT_EMAIL}
+  git config --global user.name ${GIT_USERNAME}
   git config --global core.editor vim
   git config --global color.ui true
   git config --global color.diff auto
@@ -45,16 +34,43 @@ function config_git() {
   git config --global color.branch auto
 }
 
-function config_JS_env() {}
+function config_JS_env() {
+  echo -e "\n${COLOR}_____ Setting up JavaScript environment _____${NO_COLOR}\n"
 
-function symlink_dotfiles() {}
+  brew install node
+  declare -a global_packages=(
+    @angular/cli 
+    @vue/cli 
+    eslint 
+    typescript 
+    create-react-app 
+    webpack 
+    yarn 
+    firebase-tools
+  )
+  npm i -g ${global_packages[@]}
+}
 
+function config_py_env() {
+  echo -e "\n${COLOR}_____ Setting up Python environment _____${NO_COLOR}\n"
 
-set -x -e (
-  xcode-select --install
+  brew install python3
+  sudo pip install virtualenv virtualenvwrapper
+  echo -e "export WORKON_HOME=~/.virtualenvs" >> ~/.zshrc
+  echo -e "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.zshrc
+  source ~/.zshrc
+}
 
-  source .env
+source .env
 
-  get_package_manager
-)
+xcode-select --install
 
+config_brew
+
+config zsh
+
+config_git
+
+config_JS_env
+
+config_py_env
